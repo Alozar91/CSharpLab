bfe306963a1fe0f0af620cff240eab2eb3098132
DESKTOP-SF4AQEQ\SeregaPC <sermer@list.ru>
2025-05-26 14:34:09 +0300

    +Лабораторная 1

--- Lab1.1/Program.cs
+++ Lab1.1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Lab1._1
+﻿using System.Threading;
+namespace Lab1._1
 {
+    public class MyThread
+    {
+        public required int Start { get; init; }
+        public required int End { get; init; }
+        public void Print()
+        {
+            for (int i = Start; i <= End; i++){
+                Console.WriteLine($"Thread {Thread.CurrentThread.Name} - {i}");
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var t1 = new Thread(new MyThread() { Start = 1, End = 100 }.Print) { Name="A"};
+            var t2 = new Thread(new MyThread() { Start = 101, End = 200 }.Print) { Name = "B" };
+
+            t1.Start();
+            t2.Start();
         }
     }
 }
